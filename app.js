c3cdbc671566848f5ba4f2774b45606982ca583b
Sandrine Poissonnet <petitspoissons@icloud.com>
2022-02-19 08:32:53 -0800

    module complete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
 	return inquirer.prompt([
@@ -141,19 +141,26 @@ Add a New Project
 		});
 };
 
-// promptUser()
-// 	.then(promptProject)
-// 	.then((portfolioData) => {
-// 		const pageHTML = generatePage(portfolioData);
+promptUser()
+	.then(promptProject)
+	.then((portfolioData) => {
+		return generatePage(portfolioData);
+	})
+	.then((pageHTML) => {
+		return writeFile(pageHTML);
+	})
+	.then((writeFileResponse) => {
+		console.log(writeFileResponse);
+		return copyFile();
+	})
+	.then((copyFileResponse) => {
+		console.log(copyFileResponse);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
-// 		// fs.writeFile('./index.html', pageHTML, (err) => {
-// 		// 	if (err) throw new Error(err);
-// 		// 	console.log(
-// 		// 		'Portfolio complete! Check out index.html to see the output!'
-// 		// 	);
-// 		// });
-// 	});
-
+/*
 const mockData = {
 	name: 'Lernantino',
 	github: 'lernantino',
@@ -199,9 +206,4 @@ const mockData = {
 		},
 	],
 };
-
-const pageHTML = generatePage(mockData);
-fs.writeFile('./index.html', pageHTML, (err) => {
-	if (err) throw new Error(err);
-	console.log('Portfolio complete! Check out index.html to see the output!');
-});
+*/
